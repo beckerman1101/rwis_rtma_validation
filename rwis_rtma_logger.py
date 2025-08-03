8f5fc5efd5cfb6e56eaa2bdfb342e19b598f57e1
beckerman1101 <143034659+beckerman1101@users.noreply.github.com>
2025-08-03 12:51:06 -0600

    Update rwis_rtma_logger.py

--- rwis_rtma_logger.py
+++ rwis_rtma_logger.py
@@ -171,8 +171,7 @@ def interpolate_rtma_to_points(grib_file: str, rwis: pd.DataFrame) -> pd.DataFra
 SELECTED_STATIONS = {
     'W206', 'W224', 'W199', 'W195', 'W211',
     'E171', 'E238', 'E237', 'E227', 'E235', 
-    'E234', 'E240', 'E213', 'E232', 'W221',
-    'E216'
+    'E234', 'E240', 'E213', 'E232', 'W221'
 }
 
 # Add this constant near the top with your other configuration constants
@@ -649,6 +648,7 @@ def main() -> None:
 
 if __name__ == "__main__":
     main()
+
 
 
 
